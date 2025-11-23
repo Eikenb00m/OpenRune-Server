bf03d1a2a960baa3885db3951ef4cad463d1970d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-11-23 19:02:50 +0100

    Use forced movement for log balance

--- content/src/main/kotlin/org/alter/skills/agility/GnomeStrongholdCoursePlugin.kt
+++ content/src/main/kotlin/org/alter/skills/agility/GnomeStrongholdCoursePlugin.kt
@@ -5,9 +5,10 @@ import org.alter.api.ext.addXp
 import org.alter.api.ext.filterableMessage
 import org.alter.api.ext.popRenderAnim
 import org.alter.api.ext.pushRenderAnim
+import org.alter.game.model.Direction
+import org.alter.game.model.ForcedMovement
 import org.alter.game.model.Tile
 import org.alter.game.model.entity.Player
-import org.alter.game.model.move.MovementQueue
 import org.alter.game.pluginnew.PluginEvent
 import org.alter.game.pluginnew.event.impl.onObjectOption
 
@@ -25,13 +26,20 @@ class GnomeStrongholdCoursePlugin : PluginEvent() {
         onObjectOption(LOG_BALANCE_ID, "walk-across") {
             player.queue {
                 val distance = player.tile.getDistance(LOG_DESTINATION)
+                val duration = distance * 30
+                val movement = ForcedMovement.of(
+                    src = player.tile,
+                    dst = LOG_DESTINATION,
+                    clientDuration1 = duration,
+                    clientDuration2 = duration,
+                    directionAngle = Direction.SOUTH.angle,
+                )
                 try {
                     player.lock()
                     player.filterableMessage("You walk carefully across the slippery log...")
                     player.pushRenderAnim("sequences.human_walk_logbalance")
-                    player.movementQueue.clear()
-                    player.movementQueue.addStep(LOG_DESTINATION, MovementQueue.StepType.FORCED_WALK)
-                    wait(distance + 2)
+                    player.faceTile(LOG_DESTINATION)
+                    player.forceMove(this, movement)
                     player.filterableMessage("... and make it safely to the other side.")
                     player.addXp(Skills.AGILITY, LOG_BALANCE_XP)
                 } finally {
